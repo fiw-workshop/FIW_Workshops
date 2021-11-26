3c06bda520bb6fb48938c39dc611df3e9a0f54f2
Bettina <BettinaMueller@pm.me>
2021-11-26 18:19:38 +0100

    refactoring code and fixing some minor bugs

--- Ich_Statement_PWA/data.js
+++ Ich_Statement_PWA/data.js
@@ -1,114 +1,125 @@
-function getData(){
+function getData() {
     document.getElementById("feeling").innerHTML += localStorage.getItem("selectedFeeling");
     let selectedTopic = localStorage.getItem("selectedTopic")
     let selectedOption = localStorage.getItem("selectedOption")
-    if(selectedTopic === "friends"){
-        if(selectedOption === "gossip"){
-            document.getElementById("explanation").innerHTML += "du über mich gelästert hast."
-            document.getElementById("question").innerHTML += "Kannst du das bitte nicht mehr tun?"
-        }
-        if(selectedOption === "contact"){
-            document.getElementById("explanation").innerHTML += "du dich nicht bei mir gemeldet hast."
-            document.getElementById("question").innerHTML += "Kannst du dich bitte öfters bei mir melden?"
-        }
-        if(selectedOption === "time"){
-            document.getElementById("explanation").innerHTML += "du mehr Zeit mit anderen verbringst als mit mir."
-            document.getElementById("question").innerHTML += "Wie können wir mehr Zeit miteinander verbringen?"
-        }
+    if (selectedTopic === "friends") {
+        create_friendship_statement();
+    } else if (selectedTopic === "boss") {
+        create_boss_statement();
+    } else if (selectedTopic === "mailman") {
+        create_mailman_statement();
+    } else if (selectedTopic === "partner") {
+        create_partner_statement();
+    } else if (selectedTopic === "family") {
+        create_family_statement();
+    } else if (selectedTopic === "teacher") {
+        create_teacher_statement();
     }
-    else if(selectedTopic === "boss"){
-        if(selectedOption === "overtime"){
+    function create_boss_statement(){
+        if (selectedOption === "overtime") {
             document.getElementById("explanation").innerHTML += "Sie mir zu viele Überstunden geben."
             document.getElementById("question").innerHTML += "Können Sie zukünftig auf meine Zeiten achten?"
-        }
-        if(selectedOption === "appreciation"){
+        } else if (selectedOption === "appreciation") {
             document.getElementById("explanation").innerHTML += "meine Arbeit nicht wertgeschätzt wird."
             document.getElementById("question").innerHTML += "Können Sie mir öfters Feedback geben?"
-        }
-        if(selectedOption === "privacy"){
+        } else if (selectedOption === "privacy") {
             document.getElementById("explanation").innerHTML += "Sie mein Privatleben nicht respektieren."
             document.getElementById("question").innerHTML += "Können Sie zukünftig auf mein Privatleben achten?"
         }
     }
-    else if(selectedTopic === "mailman"){
-        if(selectedOption === "ringing"){
+    function create_mailman_statement() {
+        if (selectedOption === "ringing") {
             document.getElementById("explanation").innerHTML += "Sie nicht bei mir geklingelt haben."
             document.getElementById("question").innerHTML += "Können Sie bitte das nächste Mal bei mir klingeln?"
         }
-        if(selectedOption === "yelling"){
+        if (selectedOption === "yelling") {
             document.getElementById("explanation").innerHTML += "Sie mich angeschrien haben."
             document.getElementById("question").innerHTML += "Können Sie bitte freundlicher sein?"
         }
-        if(selectedOption === "neighbour"){
+        if (selectedOption === "neighbour") {
             document.getElementById("explanation").innerHTML += "Sie mein Paket nicht bei meinem Nachbarn abgegeben haben."
             document.getElementById("question").innerHTML += "Können Sie mein Paket bitte das nächste Mal bei meinem Nachbarn abgeben?"
         }
     }
-    else if(selectedTopic === "partner"){
-        if(selectedOption === "jealousy"){
+    function create_partner_statement() {
+        if (selectedOption === "jealousy") {
             document.getElementById("explanation").innerHTML += "du versucht hast mich eifersüchtig zu machen."
             document.getElementById("question").innerHTML += "Kannst du das bitte nicht mehr tun?"
         }
-        if(selectedOption === "lied"){
+        if (selectedOption === "lied") {
             document.getElementById("explanation").innerHTML += "du mich angelogen hast."
             document.getElementById("question").innerHTML += "Kannst du das nächste Mal bitte ehrlich sein?"
         }
-        if(selectedOption === "birthday"){
+        if (selectedOption === "birthday") {
             document.getElementById("explanation").innerHTML += "du meinen Geburtstag vergessen hast."
             document.getElementById("question").innerHTML += "Kannst du dir meinen Geburtstag in deinen Kalender schreiben?"
         }
     }
-    else if(selectedTopic === "family"){
-        if(selectedOption === "clothes"){
+    function create_family_statement() {
+        if (selectedOption === "clothes") {
             document.getElementById("explanation").innerHTML += "du mein Oberteil geklaut hast."
             document.getElementById("question").innerHTML += "Kannst du mich bitte das nächste Mal vorher fragen?"
         }
-        if(selectedOption === "nagging"){
+        if (selectedOption === "nagging") {
             document.getElementById("explanation").innerHTML += "du mich angemeckert/angeschrien hast."
             document.getElementById("question").innerHTML += "Kannst du bitte netter zu mir sein?"
         }
-        if(selectedOption === "picture"){
+        if (selectedOption === "picture") {
             document.getElementById("explanation").innerHTML += "du ein peinliches Bild von mir gezeigt hast."
             document.getElementById("question").innerHTML += "Kannst du bitte keine Bilder mehr von mir zeigen?"
         }
     }
-    else if(selectedTopic === "teacher"){
-        if(selectedOption === "grades"){
+    function create_teacher_statement() {
+        if (selectedOption === "grades") {
             document.getElementById("explanation").innerHTML += "Sie mir eine schlechte Note gegeben haben."
             document.getElementById("question").innerHTML += "Können wir nochmal über meine Note sprechen?"
         }
-        if(selectedOption === "questioning"){
+        if (selectedOption === "questioning") {
             document.getElementById("explanation").innerHTML += "Sie mich drangenommen haben."
             document.getElementById("question").innerHTML += "Können Sie die Frage noch einmal genauer erklären?"
         }
-        if(selectedOption === "time"){
+        if (selectedOption === "time") {
             document.getElementById("explanation").innerHTML += "Sie die Zeit überzogen haben."
             document.getElementById("question").innerHTML += "Können Sie sich bitte nächstes Mal an die Zeiten halten?"
+        }
+    }
+    function create_friendship_statement() {
+        if (selectedOption === "gossip") {
+            document.getElementById("explanation").innerHTML += "du über mich gelästert hast."
+            document.getElementById("question").innerHTML += "Kannst du das bitte nicht mehr tun?"
+        }
+        if (selectedOption === "contact") {
+            document.getElementById("explanation").innerHTML += "du dich nicht bei mir gemeldet hast."
+            document.getElementById("question").innerHTML += "Kannst du dich bitte öfters bei mir melden?"
+        }
+        if (selectedOption === "time") {
+            document.getElementById("explanation").innerHTML += "du mehr Zeit mit anderen verbringst als mit mir."
+            document.getElementById("question").innerHTML += "Wie können wir mehr Zeit miteinander verbringen?"
         }
     }
 }
 
 const imagesArray = ["cat.jpg", "Cookie.jpg", "Dinosauriern.jpg", "Faultier.jpg", "Giraffe.jpg"];
 
-function displayImage(){
+function displayImage() {
     let number = Math.floor(Math.random() * imagesArray.length);
-    document.getElementById("picture").src = "../images/happy_images/" + imagesArray[number];
+    document.getElementById("picture").src = "images/happy_images/" + imagesArray[number];
 }
 
-function generateFeeling(elementId){
+function generateFeeling(elementId) {
     let feeling = document.getElementById(elementId).innerText;
     localStorage.setItem('selectedFeeling', feeling)
 }
 
-function loadTopic(){
+function loadTopic() {
     let topic = localStorage.getItem("selectedTopic");
     window.location.href = 'topics/' + topic + '.html';
 }
 
-function storeTopic(topic){
+function storeTopic(topic) {
     localStorage.setItem('selectedTopic', topic)
 }
 
-function storeOption(option){
+function storeOption(option) {
     localStorage.setItem('selectedOption', option)
 }
--- Ich_Statement_PWA/sw.js
+++ Ich_Statement_PWA/sw.js
@@ -1,31 +1,41 @@
 self.addEventListener("install", e => {
     e.waitUntil(
-      caches.open("static").then(cache => {
-          return cache.addAll([
-              "./",
-              "topic.html",
-              "feeling.html",
-              "./topics/boss.html",
-              "./topics/family.html",
-              "./topics/friends.html",
-              "./topics/mailman.html",
-              "./topics/partner.html",
-              "./topics/teacher.html",
-              "final_statement.html",
-              "./src/master.css",
-              "./images/I_Statement_Logo_192.png",
-              "./images/I_Statement_Logo_512.png",
-              "./images/start_page/startseite1.jpg"
-          ]);
-      })
+        caches.open("static").then(cache => {
+            return cache.addAll([
+                "./",
+                "favicon.ico",
+                "choosing_topic.html",
+                "choosing_feeling.html",
+                "index.html",
+                "data.js",
+                "manifest.json",
+                "./topics/boss.html",
+                "./topics/family.html",
+                "./topics/friends.html",
+                "./topics/mailman.html",
+                "./topics/partner.html",
+                "./topics/teacher.html",
+                "final_statement.html",
+                "final.html",
+                "./src/master.css",
+                "./images/I_Statement_Logo_192.png",
+                "./images/I_Statement_Logo_512.png",
+                "./images/start_page/startseite1.jpg",
+                "./images/happy_images/cat.jpg",
+                "./images/happy_images/Cookie.jpg",
+                "./images/happy_images/Dinosauriern.jpg",
+                "./images/happy_images/Faultier.jpg",
+                "./images/happy_images/Giraffe.jpg",
+            ]);
+        })
     );
     console.log("Installed!");
 });
 
-self.addEventListener("fetch", e => {
-   e.respondWith(
-       caches.match(e.request).then(response => {
-           return response || fetch(e.request);
-       })
-   );
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
