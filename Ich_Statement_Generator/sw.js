29f1079696e44879bba4773f953641b880dcf834
mahsasaifi <mahsa.saifi@student.htw-berlin.de>
2021-12-13 23:26:09 +0100

    Ich_Statement_Generator für das Tutorial

---
+++ Ich_Statement_Generator/sw.js
@@ -0,0 +1,56 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll([
+                "./",
+                "favicon.ico",
+                "choosing_topic.html",
+                "choosing_feeling.html",
+                "index.html",
+                "data.js",
+                "manifest.json",
+                "./topics/boss.html",
+                "./topics/family.html",
+                "./topics/friends.html",
+                "./topics/mailman.html",
+                "./topics/partner.html",
+                "./topics/teacher.html",
+                "final_statement.html",
+                "final.html",
+                "./css/master.css",
+                "./images/I_Statement_Logo_192.png",
+                "./images/I_Statement_Logo_512.png",
+                "./images/start_page/startseite1.jpg",
+                "./images/start_page/startseite2.jpg",
+                "./images/start_page/startseite3.jpg",
+                "./images/start_page/startseite4.jpg",
+                "./images/start_page/startseite5.jpg",
+                "./images/start_page/startseite6.jpg",
+                "./images/happy_images/cat.jpg",
+                "./images/happy_images/Cookie.jpg",
+                "./images/happy_images/Dinosauriern.jpg",
+                "./images/happy_images/Faultier.jpg",
+                "./images/happy_images/Giraffe.jpg",
+                "./images/happy_images/Hase.jpg",
+                "./images/happy_images/Hotdog.jpg",
+                "./images/happy_images/kang.jpg",
+                "./images/happy_images/kang2.jpg",
+                "./images/happy_images/Katze.jpg",
+                "./images/happy_images/Koala.jpg",
+                "./images/happy_images/Mops.jpg",
+                "./images/happy_images/Orange.jpg",
+                "./images/happy_images/Panda.jpg",
+                "./images/happy_images/Pinguin.jpg",
+            ]);
+        })
+    );
+    console.log("Installed!");
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
